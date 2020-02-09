4b7abfe92dcbcb81d25438adb8862a0dbf0ad1ea
vallejogalileo11 <60833939+vallejogalileo11@users.noreply.github.com>
2020-02-08 21:45:06 -0600

    Create class1.py

---
+++ class1.py
@@ -0,0 +1,41 @@
+ PI = 3.14
+print(PI)
+
+one = 1 #Forma comun de asignar variables.
+two = 2
+three = 3
+
+one, two, three = 1,2,3 #Forma resumida de asignar variables.
+
+print(one)
+print(two)
+print(three)
+two = 4 #Darle un nuevo valor a la variable (en este caso a two)
+print(two)
+print(one)
+
+Decimal= 1.1 (es un float o un valor de punto flotante.)
+print(Decimal)
+
+StringVar = "Hello" + "1" #Como coloqué string en (Hello) el (1) debe de ir también en string
+print(StringVar)
+
+def FunctionName ():
+     newVar = "world"
+     print(one)
+     print(newVar)
+     return 
+
+FunctionName() #Imprime el conjunto de (def FunctionName)
+
+five = 3+2
+print(five)
+
+count = 3
+print(count)
+count = count + 1 #esto es igual a (count += 1)
+print(count)
+count = count + 1
+print(count) #Se va actualizando el valor con la nueva variable
+
+#count = count * 5 (esto es igual a count *= 5)
